6ab066519fb26dd09c330148837894cdfd6b7877
emadghods <emad.ghods@gmail.com>
2020-06-30 16:38:56 +0430

    Initial commit

---
+++ src/Test.java
@@ -0,0 +1,13 @@
+public class Test {
+    public static void main(String[] args) {
+        System.out.println("testt");
+    }
+}
+
+class haminjuri {
+
+    public static void main(String[] args) {
+
+    }
+
+}
